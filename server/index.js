12780ec2ce8f5950e8739d26986b28a94dd46d4d
Brian Poole <brianpooledev@gmail.com>
2018-02-21 08:44:14 -0600

    prepare for server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const controller = require("./controllers/control.js");
 
 const app = express();
 
+app.use(express.static(`${__dirname}/../build`));
 app.use(cors());
 app.use(json());
 
@@ -14,6 +15,11 @@ app.get("/api/getApiData", controller.getApiData);
 app.put("/api/updateCoin/:index/:name", controller.updateCoin);
 app.delete("/api/removeCoin/:index/:name", controller.removeCoin);
 app.post("/api/createCoin", controller.createCoin);
+
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
